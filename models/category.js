a659040570314f7405c9c9f43bc269559ffda591
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-07 16:36:13 +0200

    feat: add homepage and display all records

--- models/category.js
+++ models/category.js
@@ -5,6 +5,10 @@ const categorySchema = new Schema({
     type: String,
     required: true,
   },
+  icon: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const Record = require('../../models/record');
+const Category = require('../../models/category');
 
-router.get('/', (req, res) => {
-  res.render('index');
+function formatDate(date) {
+  return (formattedDate = new Intl.DateTimeFormat('zh-Hans').format(date));
+}
+
+router.get('/', async (req, res) => {
+  try {
+    const records = await Record.find().lean().sort({ date: 'desc' });
+    const categories = await Category.find().lean();
+    const totalExpense = records.reduce(
+      (total, record) => record.amount + total,
+      0
+    );
+    const formattedRecords = records.map((record) => {
+      return {
+        ...record,
+        date: formatDate(record.date),
+        category: categories.find(
+          (el) => String(el._id) === String(record.categoryId)
+        ),
+      };
+    });
+    if (formattedRecords) {
+      res.render('index', { records: formattedRecords, totalExpense });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
