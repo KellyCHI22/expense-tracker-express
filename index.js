d0075c7f6757aedb396bab4715785229f717bde1
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-07 16:54:32 +0200

    feat: add express-session

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const routes = require('./routes');
 
 require('./config/mongoose');
@@ -7,10 +8,19 @@ require('./config/mongoose');
 const app = express();
 const port = 3000;
 
-// setting template engine
+// set template engine
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' })); // extname: '.hbs'，是指定副檔名為 .hbs，有了這行以後，我們才能把預設的長檔名改寫成短檔名
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
+
+// set session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // setting body-parser
 app.use(express.urlencoded({ extended: true }));
