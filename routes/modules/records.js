1611dfe408501e7f42d555dae2661ffe3e4fa02f
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-07 18:15:07 +0200

    feat: add new and edit routes and views

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../../models/category');
 
-router.get('/', (req, res) => {
-  res.send('this is records route');
+router.get('/new', async (req, res) => {
+  try {
+    const categories = await Category.find().lean();
+    res.render('new', { categories });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get('/:record_id/edit', (req, res) => {
+  res.render('edit');
 });
 
 module.exports = router;
