d9232784977eed4a3ed3ed6295e7f982c526a30c
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-07 12:01:18 +0200

    feat: init project

---
+++ index.js
@@ -0,0 +1,28 @@
+// app.js
+// include packages and define server related variables
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+// setting template engine
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' })); // extname: '.hbs'，是指定副檔名為 .hbs，有了這行以後，我們才能把預設的長檔名改寫成短檔名
+app.set('view engine', 'hbs');
+
+// setting body-parser
+app.use(express.urlencoded({ extended: true }));
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/', (req, res) => {
+  res.render('index');
+});
+
+// starts the express server and listening for connections.
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`);
+});
