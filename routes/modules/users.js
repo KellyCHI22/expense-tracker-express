389cc25f1cfd970ef2056beea422f389278d38b4
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-07 17:17:26 +0200

    feat: add logout route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -64,4 +64,14 @@ router.post('/register', (req, res) => {
   });
 });
 
+// https://stackoverflow.com/questions/72336177/error-reqlogout-requires-a-callback-function
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
