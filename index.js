d1e6d01cf791be3c78ab44e002629ddf724427d8
Hsin-tingCHI <chihsinting@gmail.com>
2023-07-09 16:08:06 +0200

    fix: small modifications for deploying

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const usePassport = require('./config/passport');
 require('./config/mongoose');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3030;
 
 // set template engine
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' })); // extname: '.hbs'，是指定副檔名為 .hbs，有了這行以後，我們才能把預設的長檔名改寫成短檔名
@@ -40,6 +40,6 @@ app.use((req, res, next) => {
 app.use(routes);
 
 // starts the express server and listening for connections.
-app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Express is running on http://localhost:${PORT}`);
 });
